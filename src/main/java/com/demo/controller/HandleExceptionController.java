68a56ef43e26aa72364767e4c801ccbdbb7eeed5
Assanai Manurat <assanai17@gmail.com>
2017-07-18 07:10:07 +0700

    update aspect handle exception

--- src/main/java/com/demo/controller/HandleExceptionController.java
+++ src/main/java/com/demo/controller/HandleExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -21,7 +22,7 @@ public class HandleExceptionController {
         return new ResponseEntity<>(e.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(value = {LoginFailException.class})
+    @ExceptionHandler(value = {LoginFailException.class, BadCredentialsException.class})
     public ResponseEntity<Object> loginFailException(LoginFailException e) {
         System.out.println("-- this is login fail exception --");
         return new ResponseEntity<>(e.getMessage(), new HttpHeaders(), HttpStatus.UNAUTHORIZED);
@@ -40,7 +41,7 @@ public class HandleExceptionController {
 
     @ExceptionHandler({AccessDeniedException.class})
     public ResponseEntity<Object> handleAccessDeniedException(Exception ex, WebRequest request) {
-        return new ResponseEntity<>("Access denied message here", new HttpHeaders(), HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>("Access denied ", new HttpHeaders(), HttpStatus.FORBIDDEN);
     }
 
 }
