52f25918c666058adc67ed12e554a95e41c5d847
Assanai Manurat <assanai17@gmail.com>
2017-07-18 07:07:23 +0700

    implement CommandLineRunner and Enable Global Method Security

--- src/main/java/com/demo/SpringSecurityJwtDemoApplication.java
+++ src/main/java/com/demo/SpringSecurityJwtDemoApplication.java
@@ -1,10 +1,30 @@
 package com.demo;
 
+import com.demo.domain.User;
+import com.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SpringSecurityJwtDemoApplication {
+
+	@Autowired private UserRepository userRepository;
+
+	@Bean
+	public CommandLineRunner initCommandLineRunner() {
+		return args -> {
+			final User entity = new User("assanai", "manurat");
+			userRepository.save(entity);
+
+			final User userResult = userRepository.findByUsername("assanai");
+			System.out.println(userResult.getUsername() + " "+ userResult.getPassword());
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtDemoApplication.class, args);
