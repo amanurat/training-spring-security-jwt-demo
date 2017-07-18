bbadda2c15cd2696501f9a00f8e10b89ce841cdb
Assanai Manurat <assanai17@gmail.com>
2017-07-18 07:11:17 +0700

    add domain User , Authority , UserRepository

---
+++ src/main/java/com/demo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.demo.repository;
+
+import com.demo.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+    User findByUsernameAndPassword(String username, String password);
+}
