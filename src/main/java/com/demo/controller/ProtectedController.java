744dd944b7e39929de556d28d3e2adcbcc9911ae
Assanai Manurat <assanai17@gmail.com>
2017-07-18 07:10:26 +0700

    apply security method

--- src/main/java/com/demo/controller/ProtectedController.java
+++ src/main/java/com/demo/controller/ProtectedController.java
@@ -1,6 +1,7 @@
 package com.demo.controller;
 
 import com.demo.bean.UserDetail;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,23 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class ProtectedController {
 
     @RequestMapping(value = "/protected/1", method = GET)
+    @Secured("ROLE_USER")
     public void protectedPath(Authentication authentication) {
 
         final UserDetail principal = (UserDetail) authentication.getPrincipal();
         System.out.println(principal.getUsername());
 
-        System.out.println("-- protected --");
+        System.out.println("-- protected for ROLE_USER --");
+    }
+
+    @RequestMapping(value = "/protected/2", method = GET)
+    @Secured("ROLE_ADMIN")
+    public void protectedPathRoleAdmin(Authentication authentication) {
+
+        final UserDetail principal = (UserDetail) authentication.getPrincipal();
+        System.out.println(principal.getUsername());
+
+        System.out.println("-- protected for ROLE_ADMIN --");
     }
 
 }
